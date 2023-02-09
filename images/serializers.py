e08be9383548608d342b1173a941b728e09c8d1c
keonoh00 <keonoh00@gmail.com>
2023-02-09 14:53:31 +0900

    Use ModelSerializer instead of manual declaration using Serializer

--- images/serializers.py
+++ images/serializers.py
@@ -3,19 +3,39 @@ from .image_classes import CIFAR100ImageClasses
 from .models import Image
 
 
-class ImageSerializer(serializers.Serializer):
-    image_name = serializers.CharField(max_length=140)
-    image = serializers.ImageField()
-    created_by = serializers.CurrentUserDefault()
-    created_at = serializers.DateTimeField(read_only=True)
-    category = serializers.ChoiceField(choices=CIFAR100ImageClasses.choices)
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = (
+            "image_name",
+            "image",
+            "category",
+        )
+        """ Or we can use exclude instead of fields: """
+        # exclude = (
+        #     "created_by",
+        #     "created_at",
+        # )
 
-    def create(self, validated_data):
-        return Image.objects.create(**validated_data)
+        """ If all fields should be editable, use the following code: """
+        # fields = "__all__"
 
-    def update(self, instance, validated_data):
-        instance.image_name = validated_data.get("image_name", instance.image_name)
-        instance.image = validated_data.get("image", instance.image)
-        instance.category = validated_data.get("category", instance.category)
-        instance.save()
-        return instance
+
+"""Using Serializer"""
+
+# class ImageSerializer(serializers.Serializer):
+#     image_name = serializers.CharField(max_length=140)
+#     image = serializers.ImageField()
+#     created_by = serializers.CurrentUserDefault()
+#     created_at = serializers.DateTimeField(read_only=True)
+#     category = serializers.ChoiceField(choices=CIFAR100ImageClasses.choices)
+
+#     def create(self, validated_data):
+#         return Image.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.image_name = validated_data.get("image_name", instance.image_name)
+#         instance.image = validated_data.get("image", instance.image)
+#         instance.category = validated_data.get("category", instance.category)
+#         instance.save()
+#         return instance
