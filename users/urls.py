d13ea88b6dddb6a9aafe459915c09248e4913242
keonoh00 <keonoh00@gmail.com>
2023-02-17 15:07:40 +0900

    Change create user url

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("me", views.Me.as_view()),
-    path("create-user", views.CreateUser.as_view()),
+    path("create-account", views.CreateUser.as_view()),
     path("change-password", views.ChangePassword.as_view()),
     path("login", views.Login.as_view()),
     path("logout", views.Logout.as_view()),
