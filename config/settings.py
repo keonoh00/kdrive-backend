4e95ca4977e5c42fd35697b18c153787e2ab1dbc
keonoh00 <keonoh00@gmail.com>
2023-02-28 23:28:06 +0900

    Update lock

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ DEBUG = "RENDER" not in os.environ
 ALLOWED_HOSTS = [
     "localhost",
     "kdrive.onrender.com",
+    "127.0.0.1",
 ]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
